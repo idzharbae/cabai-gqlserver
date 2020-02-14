c0d685e2539d9cc8c4b654e8300a0c86548b1ccc
muhammad idzhar darissalam <idzharbae@gmail.com>
2020-02-14 07:54:14 +0700

    refactor module resolver

--- gql/cabaicatalog/handler.go
+++ gql/cabaicatalog/handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/idzharbae/cabai-gqlserver/gql/cabaicatalog/fetcher"
 	"github.com/idzharbae/cabai-gqlserver/gql/cabaicatalog/requests"
+	"github.com/idzharbae/cabai-gqlserver/gql/cabaicatalog/resolver"
 )
 
 type CabaiCatalogHandler struct {
@@ -16,8 +17,8 @@ func NewCabaiCatalogHandler(productReader fetcher.ProductReader) *CabaiCatalogHa
 
 func (r *CabaiCatalogHandler) Products(ctx context.Context, args struct {
 	Params requests.ListProduct
-}) (*[]*Product, error) {
+}) (*[]*resolver.Product, error) {
 	res := r.productReader.ListProducts(context.Background(), args.Params)
-	products := NewProducts(res)
+	products := resolver.NewProducts(res)
 	return &products, nil
 }
--- gql/cabaicatalog/resolver/product.go
+++ gql/cabaicatalog/resolver/product.go
@@ -1,4 +1,4 @@
-package cabaicatalog
+package resolver
 
 import (
 	"github.com/graph-gophers/graphql-go"
