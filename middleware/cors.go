8749fb51f98bae469efe3aa5199dc1ab26e3f088
muhammad idzhar darissalam <idzharbae@gmail.com>
2020-03-27 18:45:06 +0700

    add additional cors headers

--- middleware/cors.go
+++ middleware/cors.go
@@ -6,7 +6,8 @@ func CorsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// TODO allow only from whitelisted servers in config
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 		next.ServeHTTP(w, r)
 	})
 }
