86ab2140a9435b3a09d5126df8135b11cb96f6a9
muhammad idzhar darissalam <idzharbae@gmail.com>
2020-03-28 21:12:09 +0700

    handle preflight requests

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,6 +8,10 @@ func CorsMiddleware(next http.Handler) http.Handler {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
 		next.ServeHTTP(w, r)
 	})
 }
